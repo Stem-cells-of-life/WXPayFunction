1190a7defd2a9aec7b234afc6b7b068878a09be8
Stem-cells-of-life <41986597+Stem-cells-of-life@users.noreply.github.com>
2018-08-10 12:44:25 +0800

    修改

---
+++ weixin/src/com/sccbb/servlet/SaveFeeLogin.java
@@ -0,0 +1,74 @@
+package com.sccbb.servlet;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.HashMap;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.sccbb.dao.GetConnection;
+
+
+public class SaveFeeLogin extends HttpServlet {
+					
+	
+					@Override
+					protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+							throws ServletException, IOException {
+							doPost(req, resp);
+					}
+					
+					@Override
+					protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+					throws ServletException, IOException {
+							resp.setContentType("text/html; charset=UTF-8");
+							req.setCharacterEncoding("UTF-8");
+							String username = req.getParameter("username").toUpperCase().trim();
+							String sfznum = req.getParameter("sfznum").toUpperCase().trim();
+							System.out.println(username+sfznum);
+							if(("".equals(username)||username==null)||("".equals(sfznum)||sfznum==null)||
+							(!getUserMsg(username, sfznum))){
+									req.getRequestDispatcher("erro1.html").forward(req, resp);
+							}else{
+								req.getRequestDispatcher("success.html").forward(req, resp);
+								}
+					}
+					
+							
+					public Boolean getUserMsg(String username,String sfznum){
+						GetConnection getCon=new GetConnection();
+						Connection conn = getCon.getConnection();
+						PreparedStatement ps =null;
+						ResultSet rs  =null;
+						String sql="select BS_AGREEMENT_0025,BS_AGREEMENT_0034 from SCCBB_BS_AGREEMENT where BS_AGREEMENT_0025=? and BS_AGREEMENT_0034=? ";
+						try {
+										ps  = conn.prepareStatement(sql);
+										ps.setString(1,username);
+										ps.setString(2,sfznum);
+							rs = ps.executeQuery();
+							if(rs.next()){
+								return true;
+								}else{
+									return false;
+								}
+						} catch (SQLException e) {
+							e.printStackTrace();
+						}finally{
+							try {
+								ps.close();
+								rs.close();
+								conn.close();
+							} catch (SQLException e) {
+								e.printStackTrace();
+							}	
+						}
+						return false;
+					}
+}
