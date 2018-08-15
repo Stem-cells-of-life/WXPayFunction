ded10e8eadf84291f21a5e5d83f2b49754db8ed5
panda2wa <13308222033@163.com>
2018-08-15 09:23:58 +0800

    修改

--- weixin/src/com/sccbb/servlet/SaveFeeLogin.java
+++ weixin/src/com/sccbb/servlet/SaveFeeLogin.java
@@ -1,6 +1,7 @@
 package com.sccbb.servlet;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -28,17 +29,34 @@ public class SaveFeeLogin extends HttpServlet {
 					@Override
 					protected void doPost(HttpServletRequest req, HttpServletResponse resp)
 					throws ServletException, IOException {
-							resp.setContentType("text/html; charset=UTF-8");
+							resp.setContentType("application/json; charset=UTF-8");//ajax 返回json的设置
+//							resp.setHeader("Access-Control-Allow-Origin","*");//跨域问题的解决办法
 							req.setCharacterEncoding("UTF-8");
-							String username = req.getParameter("username").toUpperCase().trim();
-							String sfznum = req.getParameter("sfznum").toUpperCase().trim();
-							System.out.println(username+sfznum);
-							if(("".equals(username)||username==null)||("".equals(sfznum)||sfznum==null)||
-								(!getUserMsg(username, sfznum))){
-								req.getRequestDispatcher("erro1.html").forward(req, resp);
-							}else{
-								req.getRequestDispatcher("success.html").forward(req, resp);
+							PrintWriter pw = null;
+							try{
+								String username = req.getParameter("username").toUpperCase().trim();
+								String sfznum = req.getParameter("sfznum").toUpperCase().trim();
+								if(("".equals(username)||username==null)||("".equals(sfznum)||sfznum==null)||
+									(!getUserMsg(username, sfznum))){
+//									req.getRequestDispatcher("erro1.html").forward(req, resp);
+									System.out.println("02");
+									String str = "{'state':'02'}";
+									pw = resp.getWriter();
+									pw.write(str);
+								}else{
+//									req.getRequestDispatcher("success.html").forward(req, resp);
+									System.out.println("01");
+									String str = "{'state':'01'}";
+									pw = resp.getWriter();
+									pw.write(str);
 								}
+							}catch (Exception e) {
+								e.printStackTrace();
+							}finally{
+								pw.flush();
+								pw.close();
+								 
+							}
 					}
 					
 							
@@ -47,7 +65,11 @@ public class SaveFeeLogin extends HttpServlet {
 						Connection conn = getCon.getConnection();
 						PreparedStatement ps =null;
 						ResultSet rs  =null;
-						String sql="select BS_AGREEMENT_0025,BS_AGREEMENT_0034 from SCCBB_BS_AGREEMENT where BS_AGREEMENT_0025=? and BS_AGREEMENT_0034=? ";
+						String sql="select " +
+									"BS_AGREEMENT_0025," +
+									"BS_AGREEMENT_0034 " +
+									"from SCCBB_BS_AGREEMENT " +
+									"where BS_AGREEMENT_0025=? and BS_AGREEMENT_0034=? ";
 						try {
 										ps  = conn.prepareStatement(sql);
 										ps.setString(1,username);
